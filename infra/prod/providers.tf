6b67e33af4b5160d94229d9e647b22703c5861b5
Dinesh Sake <dineshsake12345@gmail.com>
2026-01-28 11:10:57 +0000

    Configure remote Terraform backend

--- infra/prod/providers.tf
+++ infra/prod/providers.tf
@@ -12,3 +12,14 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "lumen-prod-terraform-state-12345"
+    key            = "prod/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
