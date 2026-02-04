d553b2c8ac868096ffdc2d6ec1b0c49b4f1c4062
Dinesh Sake <dineshsake12345@gmail.com>
2026-02-04 19:48:13 +0000

    add lambdaLogger module

---
+++ packages/observability/src/lambdaLogger.ts
@@ -0,0 +1,15 @@
+import { createLogger, format, transports } from "winston";
+
+export const lambdaLogger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
+    format.json(),
+  ),
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    }),
+  ],
+});
